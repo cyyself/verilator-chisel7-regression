d67ce2bd421d547b7627f18141e0f723522e8fd9
Yangyu Chen <cyy@cyyself.name>
2025-12-05 16:25:42 +0800

    Test.scala: add a width param

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -3,13 +3,14 @@ import chisel3.util._
 import _root_.circt.stage.ChiselStage
 
 class Test extends RawModule {
+  val width = 9 // 2**9 = 512 entries
   val io = IO(new Bundle {
-    val in = Input(Vec(16, UInt(32.W)))
-    val sel = Input(UInt(4.W))
+    val in = Input(Vec((1 << width), UInt(32.W)))
+    val sel = Input(UInt(width.W))
     val out = Output(UInt(32.W))
   })
 
-  val sel = UIntToOH(io.sel, 16)
+  val sel = UIntToOH(io.sel, 1 << width)
   io.out := Mux1H(sel, io.in)
 }
 
